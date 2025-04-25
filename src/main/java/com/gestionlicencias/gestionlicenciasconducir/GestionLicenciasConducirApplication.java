93ee0049eff46b49985a84bbdc5aa1343bbb6458
niclesanti <niclesanti687@gmail.com>
2025-04-25 18:55:45 -0300

    primer commit

---
+++ src/main/java/com/gestionlicencias/gestionlicenciasconducir/GestionLicenciasConducirApplication.java
@@ -0,0 +1,13 @@
+package com.gestionlicencias.gestionlicenciasconducir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLicenciasConducirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLicenciasConducirApplication.class, args);
+	}
+
+}
