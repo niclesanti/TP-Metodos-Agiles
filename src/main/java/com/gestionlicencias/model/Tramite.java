c84b4bad48ea84474da59a047f41951d65461fb7
francOcampo <feocampo@frsf.utn.edu.ar>
2025-05-06 12:04:36 -0300

    Clases Usuario, Tramite y TItular

---
+++ src/main/java/com/gestionlicencias/model/Tramite.java
@@ -0,0 +1,12 @@
+package com.gestionlicencias.model;
+
+import java.util.Date;
+
+public class Tramite {
+    private Integer idTramite;
+    private Date fecha;
+    private String descripcion;
+    private Float costo;
+    private Usuario usuarioResponsable;
+    private Licencia licenciaAsociada;
+}
