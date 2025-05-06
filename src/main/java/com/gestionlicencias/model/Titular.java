25e636f27d580bd2f300c4ce06849999d3596d87
niclesanti <niclesanti687@gmail.com>
2025-05-06 12:23:27 -0300

    Se agrego las anotaciones de la clase Titular

--- src/main/java/com/gestionlicencias/model/Titular.java
+++ src/main/java/com/gestionlicencias/model/Titular.java
@@ -2,15 +2,124 @@ package com.gestionlicencias.model;
 
 import java.util.Date;
 
-public class Titular {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "titular")
+public class Titular{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_titular")
     private Integer idTitular;
-    private String tipoDocumento;
-    private Long numeroDocumento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_dni", length = 20, nullable = false)
+    private TipoDni tipoDni;
+
+    @Column(name = "documento", length = 8, nullable = false, unique = true)
+    private String documento;
+
+    @Column(name = "nombre", length = 100, nullable = false)
     private String nombre;
+
+    @Column(name = "apellido", length = 100, nullable = false)
     private String apellido;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_nacimiento", nullable = false)
     private Date fechaNacimiento;
+
+    @Column(name = "direccion", length = 255)
     private String direccion;
+
+    @Column(name = "grupo_sanguineo", length = 3)
     private String grupoSanguineo;
-    private Licencia factorRH;
-    private Boolean esDonanteOrganos;
+
+    @Column(name = "factor_rh", length = 3)
+    private String factorRH;
+
+    @Column(name = "donante_organos", nullable = false)
+    private Boolean donanteOrganos;
+
+    public Integer getIdTitular() {
+        return idTitular;
+    }
+
+    public void setIdTitular(Integer idTitular) {
+        this.idTitular = idTitular;
+    }
+
+    public TipoDni getTipoDni() {
+        return tipoDni;
+    }
+
+    public void setTipoDni(TipoDni tipoDni) {
+        this.tipoDni = tipoDni;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDni(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getGrupoSanguineo() {
+        return grupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String grupoSanguineo) {
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    public String getFactorRH() {
+        return factorRH;
+    }
+
+    public void setFactorRH(String factorRH) {
+        this.factorRH = factorRH;
+    }
+
+    public Boolean getDonanteOrganos() {
+        return donanteOrganos;
+    }
+
+    public void setDonanteOrganos(Boolean donanteOrganos) {
+        this.donanteOrganos = donanteOrganos;
+    }
+
+    
 }
